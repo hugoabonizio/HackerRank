b14c48a3c698228b05337bc72e7c5021d768d6ce
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-06-07 17:22:25 -0300

    Update to python 3

--- Statistics/day1-interquartile_range.py
+++ Statistics/day1-interquartile_range.py
@@ -8,15 +8,15 @@ def build(elements, frequencies):
 def median(elements):
   size = len(elements)
   if size % 2 == 0:
-    return float(elements[size / 2] + elements[size / 2 - 1]) / 2.0
-  return elements[size / 2]
+    return float(elements[size // 2] + elements[size // 2 - 1]) / 2.0
+  return elements[size // 2]
 
-size = int(raw_input())
-elements = [int(i) for i in raw_input().split(' ')]
-frequencies = [int(i) for i in raw_input().split(' ')]
+size = int(input())
+elements = [int(i) for i in input().split(' ')]
+frequencies = [int(i) for i in input().split(' ')]
 S = build(elements, frequencies)
 
 total_size = len(S)
-q1 = median(S[0:total_size/2 - (total_size % 2)])
-q3 = median(S[total_size/2 + (total_size % 2):])
-print float(q3 - q1)
+q1 = median(S[0:(total_size // 2 - (total_size % 2))])
+q3 = median(S[(total_size // 2 + (total_size % 2)):])
+print(float(q3 - q1))
